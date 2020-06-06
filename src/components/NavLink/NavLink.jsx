23a8db9375f388d3fd91b47462db06b8b5b02a5a
Michał Wójcik <bronybracket@gmail.com>
2020-06-06 02:46:16 +0200

    styling adjustments

--- src/components/NavLink/NavLink.jsx
+++ src/components/NavLink/NavLink.jsx
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class NavLink extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <li className={this.props.className}>
