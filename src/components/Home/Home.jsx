165118bfaf23aa597483a2ffe492ba42dd3603dd
Michał Wójcik <bronybracket@gmail.com>
2020-06-09 18:16:16 +0200

    Jumbotron on homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,48 +2,25 @@ import React, { Component } from "react";
 import "./Home.css";
 import { AppBody } from "../../App";
 
+function Jumbotron() {
+  return (
+    <div className="jumbotron">
+      <h2>
+        Grupa Transportowa <span style={{ color: "brown" }}>R</span>
+        <span style={{ color: "black" }}>G</span>
+        <span style={{ color: "brown" }}>uZ</span>
+      </h2>
+      <h1>Transport z pasją!</h1>
+      <h3>Dołącz do nas!</h3>
+    </div>
+  );
+}
+
 class Home extends Component {
   render() {
     return (
       <div className="homeComponent">
-        <div className="jumbotron">
-          <h1>Homepage</h1> Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Corrupti, facere quis a asperiores ad esse aut minima dolor
-          deleniti. Voluptatum sequi dignissimos animi autem, officia modi quos
-          quasi blanditiis ratione. Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Commodi voluptatum laborum blanditiis non sunt ea
-          impedit facere sapiente obcaecati odio, ex nobis molestias numquam
-          odit aspernatur temporibus inventore rerum quae. Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Vel, veritatis illo! Sapiente ullam
-          hic beatae aliquid fuga incidunt facere omnis. Recusandae est, vitae
-          nesciunt sit eius laboriosam architecto pariatur facere. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Ipsa beatae minima odit
-          consequuntur non laudantium saepe soluta eligendi deleniti facilis a,
-          quae voluptas maxime perspiciatis aliquid qui delectus dolorum.
-          Suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit. In
-          exercitationem rem, ipsam odio recusandae accusamus? Vero, nam libero.
-          Consequatur sunt possimus quod iste blanditiis dicta nulla at natus
-          officiis repudiandae. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Pariatur nostrum suscipit dolores ipsa totam enim
-          officiis! Numquam, ipsa? Error eos facilis vel totam velit! Voluptatum
-          omnis suscipit hic voluptate earum! Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit. Ipsum eum itaque voluptatem! Quaerat
-          iusto quae ducimus quibusdam modi molestiae perferendis porro
-          consectetur alias, laboriosam pariatur amet magnam ea, quas fugiat.
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed tempore
-          sit dignissimos velit impedit odio error eum, ad commodi iste ipsam
-          ducimus cupiditate totam at neque blanditiis numquam quasi. Cum? Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. A ipsum atque
-          similique? Repudiandae id consectetur natus, cum ullam enim iste
-          nulla, quod eveniet animi accusantium repellendus dolores explicabo
-          dignissimos alias. Lorem ipsum, dolor sit amet consectetur adipisicing
-          elit. Tenetur fugiat, culpa non sapiente praesentium doloribus numquam
-          animi hic facere id saepe quasi quo quia laboriosam iure illum
-          aperiam. Laborum, cumque. Lorem ipsum, dolor sit amet consectetur
-          adipisicing elit. Temporibus optio quisquam veritatis cumque unde
-          distinctio, quo repudiandae! Odio nam fugit voluptatum accusamus
-          eaque, vero accusantium suscipit beatae! Iste, aut necessitatibus!
-        </div>
+        <Jumbotron />
         <AppBody>
           <h1>Homepage</h1> Lorem ipsum dolor sit amet consectetur adipisicing
           elit. Corrupti, facere quis a asperiores ad esse aut minima dolor
