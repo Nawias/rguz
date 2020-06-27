105803353a56eb71440fc2e31a9d6308b57660a1
Michał Wójcik <bronybracket@gmail.com>
2020-06-27 23:51:46 +0200

    InProgress sign added

--- src/components/InProgress/InProgress.jsx
+++ src/components/InProgress/InProgress.jsx
@@ -5,6 +5,7 @@ class InProgress extends Component {
   render() {
     return (
       <div className="inProgressPage">
+        <img src="img/inprogress.png" alt="InProgress" />
         <h2>Strona w budowie</h2>
         <h5>Zapraszamy już wkrótce!</h5>
       </div>
