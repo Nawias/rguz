7052ed376d2f8ef233858d2f79502389809707c1
Michał Wójcik <bronybracket@gmail.com>
2020-06-28 16:37:00 +0200

    "Our employees" carousel and two slides inside

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
+import Carousel, { Dots } from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
+import "./Work.css";
+import { AppBody } from "../../App";
 
 class Work extends Component {
   render() {
     return (
       <div className="workComponent">
-        <h1>Work</h1>
+        <AppBody>
+          <h2>Zobacz co mówią nasi pracownicy:</h2>
+        </AppBody>
+        <Carousel
+          centered
+          dots
+          infinite
+          arrows
+          autoPlay={5000}
+          animationSpeed={750}
+        >
+          <img src="img/work/zakre.png" alt="" />
+          <img src="img/work/dariusz.png" alt="" />
+          <img src="img/card-events.png" alt="" />
+        </Carousel>
+        <AppBody></AppBody>
       </div>
     );
   }
