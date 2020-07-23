93190000afb3383202aea95e7710f2430c63e22a
Michał Wójcik <bronybracket@gmail.com>
2020-07-24 00:59:10 +0200

    Added meme cards to Work component

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -3,12 +3,39 @@ import Carousel, { Dots } from "@brainhubeu/react-carousel";
 import "@brainhubeu/react-carousel/lib/style.css";
 import "./Work.css";
 import { AppBody } from "../../App";
+import { Link } from "react-router-dom";
+
+function Meme(props) {
+  return (
+    <div
+      className="meme"
+      style={{
+        "background-image": "url(" + props.img + ")",
+      }}
+    >
+      <div>{props.caption}</div>
+    </div>
+  );
+}
 
 class Work extends Component {
   render() {
     return (
       <div className="workComponent">
         <AppBody>
+          <h1 style={{ padding: "15px 10px" }}>Czy chcesz...</h1>
+          <Meme
+            img="/img/work/crash.png"
+            alt="crash"
+            caption="Jeździć po swojemu?"
+          ></Meme>
+          <h2>oraz...</h2>
+          <Meme
+            img="/img/work/cash.png"
+            alt="cash"
+            caption="Mieć kasy jak lodu?"
+          ></Meme>
+          <h1 className="joinus">Dołącz do naszej ekipy!</h1>
           <h2>Zobacz co mówią nasi pracownicy:</h2>
         </AppBody>
         <Carousel
@@ -23,7 +50,11 @@ class Work extends Component {
           <img src="img/work/dariusz.png" alt="dariusz" />
           <img src="img/work/mieszko.png" alt="mieszko" />
         </Carousel>
-        <AppBody></AppBody>
+        <AppBody>
+          <h3>
+            Napisz do nas w zakładce <Link to="/kontakt">Kontakt</Link>
+          </h3>
+        </AppBody>
       </div>
     );
   }
