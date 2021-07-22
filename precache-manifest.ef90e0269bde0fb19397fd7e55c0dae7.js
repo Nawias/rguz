d5ec9ee4138bddda16b38946690d02925cd963b2
Michał Wójcik <mjtwojcik@wp.pl>
2021-07-22 15:51:23 +0200

    Updates

---
+++ precache-manifest.ef90e0269bde0fb19397fd7e55c0dae7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25643f2c22da06f1b889420a18fdde7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcbf91799318f0a4baf6",
+    "url": "/static/css/2.66461959.chunk.css"
+  },
+  {
+    "revision": "78d3ba6d1fefad63b4a3",
+    "url": "/static/css/main.94746c39.chunk.css"
+  },
+  {
+    "revision": "fcbf91799318f0a4baf6",
+    "url": "/static/js/2.5e16304e.chunk.js"
+  },
+  {
+    "revision": "d6c686dd7b03b84eb4275c3d45df4cdc",
+    "url": "/static/js/2.5e16304e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78d3ba6d1fefad63b4a3",
+    "url": "/static/js/main.fb46b691.chunk.js"
+  },
+  {
+    "revision": "bd0b70930ddc921e9d5b",
+    "url": "/static/js/runtime-main.2cad2a61.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.99f22b07d61c90dca7c077798d157d19.js"
+  "/precache-manifest.ef90e0269bde0fb19397fd7e55c0dae7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
