0af310e7a27994b51e1245c85fc186c8e25d2e4e
Michał Wójcik <mjtwojcik@wp.pl>
2022-06-11 22:12:06 +0200

    Added UI for contact tab

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,15 +1,45 @@
-import React, { Component } from "react";
+import React, {useState} from "react";
 import "./Contact.css";
-import InProgress from "../InProgress";
+import {AppBody} from "../../App";
 
-class Contact extends Component {
-  render() {
-    return (
-      <div className="contactComponent">
-        <InProgress />
-      </div>
-    );
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const handleSendClick = (e) => {
+    setName("");
+    setEmail("");
+    setMessage("");
+    e.preventDefault();
+    e.target.blur();
   }
+  return (
+    <div className="contactComponent">
+      <AppBody>
+        <div className="panel left-panel">
+          <h1>Kontakt</h1>
+          <p>Zaintrygowany naszą ofertą? Zadzwoń, napisz lub przyjdź do biura - mamy ciasteczka!</p>
+          <p>Możesz skorzystać z formularza kontaktowego lub znaleźć nas tu:</p>
+          <p style={{textAlign:"justify"}}>
+            <b>Tel.:</b> +48 516 617 242 <br/>
+            <b>Email:</b> biuro.rguz@yandex.com <br/>
+            <br/>
+            ul. Gorzelna 17, 60-169 Poznań
+          </p>
+          <img src="/img/contact/map.png" alt="Mapa Poznania"/>
+        </div>
+        <div className="panel right-panel">
+          <h1>Napisz do nas!</h1>
+          <input type="text" className={"input singleInput"} placeholder={"Imię i Nazwisko..."} value={name} onChange={(e)=>setName(e.target.value)}/>
+          <input type="email" className={"input singleInput"} placeholder={"Email..."} value={email} onChange={(e)=>setEmail(e.target.value)}/>
+          <textarea className={"input multiInput"} placeholder={"Wiadomość..."} value={message} onChange={(e)=>setMessage(e.target.value)}/>
+          <input type="button" className={"button"} value="WYŚLIJ" onClick={handleSendClick}/>
+        </div>
+      </AppBody>
+    </div>
+  );
+
+
 }
 
 export default Contact;
